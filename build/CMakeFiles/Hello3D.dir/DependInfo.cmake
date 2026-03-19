e7e5a92169268288ffc45be2ece6a5cd6735a20d
Bruno Groehs <brunogroehs@gmail.com>
2026-03-19 13:32:50 -0300

    final

---
+++ build/CMakeFiles/Hello3D.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Bruno Groehs/Desktop/FACULDADE/Processamento Grafico/2026/Hello3D_Bruno-Groehs/Common/glad.c" "CMakeFiles/Hello3D.dir/Common/glad.c.obj" "gcc" "CMakeFiles/Hello3D.dir/Common/glad.c.obj.d"
+  "C:/Users/Bruno Groehs/Desktop/FACULDADE/Processamento Grafico/2026/Hello3D_Bruno-Groehs/src/Hello3D.cpp" "CMakeFiles/Hello3D.dir/src/Hello3D.cpp.obj" "gcc" "CMakeFiles/Hello3D.dir/src/Hello3D.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
